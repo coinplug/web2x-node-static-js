da04ebefbfceea1fb6f296649c480174a6778515
cp-jhjin <111549266+cp-jhjin@users.noreply.github.com>
2024-11-29 10:52:10 +0900

    update vrf js file

--- javascript/vrf_register_tx_v1.js
+++ javascript/vrf_register_tx_v1.js
@@ -1,23 +1,41 @@
 const vrfRegisterDataABI = [
-    {
-        "inputs": [
-          {
-            "internalType": "string",
-            "name": "_vrfData",
-            "type": "string"
-          }
-        ],
-        "name": "registerData",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "message",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "proof",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "publickey",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "random",
+        "type": "string"
+      }
+    ],
+    "name": "registerData",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
 ];
 
 async function vrfRegisterData(params) {
     const vrfRegisterDataInterface = new ethers.Interface(vrfRegisterDataABI);
     const encodedData = vrfRegisterDataInterface.encodeFunctionData('registerData', [
-        params.vrfData
+      params.message,
+      params.proof,
+      params.publickey,
+      params.random
     ]);
 
     return encodedData;
